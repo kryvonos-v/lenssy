94c31ba1a697bdd0fa50702ccb219eac7e2883a4
Volodymyr Kryvonos <volodymyr.kryvonos@gmail.com>
2022-07-07 16:02:25 +0300

    Navbar has absolute position

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,22 +1,28 @@
+import { Fragment } from 'react';
 import { NavbarBurger } from '../navbar-burger/navbar-burger';
 import { NavbarControls } from '../navbar-controls/navbar-controls';
 import './navbar.scss';
 
 export function Navbar() {
   return (
-    <header className="navbar">
-      <div className="navbar__content container">
-        <NavbarBurger className="navbar__menu-btn" />
-        <a className="navbar-logo" href="/">lens</a>
-        <span className="navbar-separator" />
-        <nav className="main-nav">
-          <a className="main-nav__link" href="/">Home</a>
-          <a className="main-nav__link" href="/tv-shows">TV Shows</a>
-          <a className="main-nav__link" href="/movies">Movies</a>
-        </nav>
+    <Fragment>
+      <div className="navbar-placeholder" />
 
-        <NavbarControls />
-      </div>
-    </header>
+      <header className="navbar">
+        <div className="navbar__content container">
+          <NavbarBurger className="navbar__menu-btn" />
+          <a className="navbar-logo" href="/">lens</a>
+          <span className="navbar-separator" />
+
+          <nav className="main-nav">
+            <a className="main-nav__link" href="/">Home</a>
+            <a className="main-nav__link" href="/tv-shows">TV Shows</a>
+            <a className="main-nav__link" href="/movies">Movies</a>
+          </nav>
+
+          <NavbarControls />
+        </div>
+      </header>
+    </Fragment>
   );
 }
